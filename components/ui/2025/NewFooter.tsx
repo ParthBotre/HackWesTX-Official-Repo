aa7eaa640ec4ab434e518a5ddd070990fc7a5319
Batuhan Sencer <dev.batuhans@gmail.com>
2025-06-04 22:42:36 -0500

    Updating the Footers background and text color

--- components/ui/2025/NewFooter.tsx
+++ components/ui/2025/NewFooter.tsx
@@ -19,53 +19,55 @@ const Footer = () => {
   );
 
   return (
-    <Grid item xs={12} overflow='noclip'>
-    <div className="w-full">
-    <div className="w-full rounded-lg flex flex-row bg-gray-900 bg-opacity-15 p-8 justify-items-center justify-stretch">
-    <Image
-    src={Logo}
-    height={400}
-    width={418}
-    className=" rounded-3xl"
-    alt="HackWesTX Logo"
-    />
-    <div className="flex flex-col justify-center content-center mx-auto w-fit justify-items-center">
-    <div className=" font-poppins text-center ">
-       <div className=" font-normal text-3xl text-center mb-5">Celebrating 5 years of HackWesTX!</div> 
-          <div className=" text-xl mb-3">Made with <div className=" inline text-yellow-700 mx-1">   ❤   </div> by HackWesTX Team</div>
-    </div>
-    <Stack
-          justifyContent="space-evenly"
-          direction="row"
-          className="text-black"
-          spacing={3}
-          width={600}
-          mt={2}
-        >
-          <IconLink
-            href="https://discord.com/invite/Y9wUhS5cKp"
-            child={<ForumIcon />}
-            color='#000000'
+    <Grid item xs={12} overflow="noclip">
+      <div className="w-full">
+        <div className="w-full rounded-lg flex flex-col md:flex-row bg-gradient-to-b from-[#020116] via-[#010010] to-[#010012] p-8 justify-center items-center gap-y-8">
+          <Image
+            src={Logo}
+            height={400}
+            width={418}
+            className="rounded-3xl"
+            alt="HackWesTX Logo"
           />
-          <IconLink
-            href="https://twitter.com/HWTXOfficial"
-            child={<Instagram />}
-            color='#000000'
-          />
-          <IconLink
-            href="https://www.depts.ttu.edu/cs/"
-            child={<SchoolIcon />}
-            color='#000000'
-          />
-          <IconLink
-            href={`mailto:contact@hackwestx.tech?subject=Quick%20HackWesTX%20Question`}
-            child={<MailIcon />}
-            color='#000000'
-          />
-        </Stack>
+          <div className="flex flex-col justify-center content-center mx-auto w-fit justify-items-center text-white text-center font-poppins">
+            <div className="font-normal text-3xl mb-5">
+              Celebrating 5 years of HackWesTX!
+            </div>
+            <div className="text-xl mb-3">
+              Made with <span className="inline text-yellow-400 mx-1">❤</span> by HackWesTX Team
+            </div>
+            <Stack
+              justifyContent="space-evenly"
+              direction="row"
+              className="text-white"
+              spacing={3}
+              width={600}
+              mt={2}
+            >
+              <IconLink
+                href="https://discord.com/invite/Y9wUhS5cKp"
+                child={<ForumIcon />}
+                color="#ffffff"
+              />
+              <IconLink
+                href="https://twitter.com/HWTXOfficial"
+                child={<Instagram />}
+                color="#ffffff"
+              />
+              <IconLink
+                href="https://www.depts.ttu.edu/cs/"
+                child={<SchoolIcon />}
+                color="#ffffff"
+              />
+              <IconLink
+                href={`mailto:contact@hackwestx.tech?subject=Quick%20HackWesTX%20Question`}
+                child={<MailIcon />}
+                color="#ffffff"
+              />
+            </Stack>
+          </div>
         </div>
-    </div>
-    </div>
+      </div>
     </Grid>
   );
 };
