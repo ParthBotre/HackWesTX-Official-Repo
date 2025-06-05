01b9eba59904798ce25982a6a789e89ccfe164aa
Batuhan Sencer <dev.batuhans@gmail.com>
2025-06-04 22:43:57 -0500

    Updating the section Header

--- components/ui/2025/sectionHeader.tsx
+++ components/ui/2025/sectionHeader.tsx
@@ -1,18 +1,15 @@
 import { Divider } from "@mantine/core";
 
-export default function SectionHeader({header}:{header:string}): JSX.Element{
-    return(
-        <>
+export default function SectionHeader({ header }: { header: string }): JSX.Element {
+  return (
+    <>
+      <Divider size="sm" className="mb-24 w-4/5 mx-auto" id={header} />
 
-        <Divider size='sm' className=" mb-24 w-4/5 mx-auto" id={header}/>
-    
-        <div className=" flex  justify-center items-center font-poppins font-semibold leading-none ">
-            
-            <div className="text-xl sm:text-2xl md:text-4xl lg:text-6xl xl:text-8xl leading-none">
-            {header}
+      <div className="flex justify-center items-center font-poppins font-semibold leading-none">
+        <div className="text-xl sm:text-2xl md:text-4xl lg:text-6xl xl:text-8xl leading-none text-white">
+          {header}
         </div>
-        </div>
-
-        </>
-        );
-        }
+      </div>
+    </>
+  );
+}
