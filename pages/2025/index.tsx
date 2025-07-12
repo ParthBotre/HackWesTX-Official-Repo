ad180ddbf951610a92a6253b6ef74e7f8b3a1db1
TalkingJupiter <dev.batuhansencer@gmail.com>
2025-07-12 10:26:44 -0500

    Updating the colors

--- pages/2025/index.tsx
+++ pages/2025/index.tsx
@@ -133,12 +133,12 @@ function Home_2025() {
                     width: "10%",
                     zIndex: 10000,
                 }}
-                href="https://mlh.io/na?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2026-season&utm_content=blue"
+                href="https://mlh.io/na?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2026-season&utm_content=white"
                 target="_blank"
                 rel="noopener noreferrer"
             >
                 <img
-                    src="https://s3.amazonaws.com/logged-assets/trust-badge/2026/mlh-trust-badge-2026-blue.svg"
+                    src="https://s3.amazonaws.com/logged-assets/trust-badge/2026/mlh-trust-badge-2026-white.svg"
                     alt="Major League Hacking 2026 Hackathon Season"
                     style={{ width: "100%" }}
                 />
