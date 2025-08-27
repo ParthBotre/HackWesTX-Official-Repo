d753dffd138fb040bc55d815719c703c1eb961d8
parthbotre <87184930+ParthBotre@users.noreply.github.com>
2025-08-27 14:24:00 -0500

    all sponsors added

--- lib/constants/sponsors-2025.ts
+++ lib/constants/sponsors-2025.ts
@@ -1,22 +1,23 @@
 import ihub from "../../assets/sponsors/ihub.png";
 // import mlt from "../../assets/sponsors/logo-mlt.png";
 // import mlh from "../../assets/sponsors/mlh-logo-color.png";
-// import pten from "../../assets/sponsors/patterson.png";
+import conetrix from "../../assets/sponsors/CNX.png";
+import pten from "../../assets/sponsors/Patterson-lite.png";
+
 // import standout from "../../assets/sponsors/stand-out-stickers-logo.png";
-// import tcl from "../../assets/sponsors/TacticalComputingLabs.png";
-
+import tcl from "../../assets/sponsors/TCL-lite.png";
 const sponsors2025 = [
-    // {
-    //     name: "Tactical Computing Labs",
-    //     avatarSrc: tcl,
-    //     link: "https://tactcomplabs.com/"
+    {
+        name: "Tactical Computing Labs",
+        avatarSrc: tcl,
+        link: "https://tactcomplabs.com/"
 
-    // },
-    // {
-    //     name: "Patterson UTI",
-    //     avatarSrc: pten,
-    //     link: "https://patenergy.com/"
-    // },
+    },
+    {
+        name: "Patterson UTI",
+        avatarSrc: pten,
+        link: "https://patenergy.com/"
+    },
     // {
     //     name: "Management Leadership for Tomorrow",
     //     avatarSrc: mlt,
@@ -37,6 +38,11 @@ const sponsors2025 = [
         avatarSrc: ihub,
         link: "https://www.depts.ttu.edu/research/research-park/"
     },
+    {
+        name: "CoNetrix",
+        avatarSrc: conetrix,
+        link: "https://conetrix.com"
+    }
 ]
 
 export default sponsors2025;
